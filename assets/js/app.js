9100a2f73cfed95211f81e30c2c58cd8a09dfd95
Terrence Mahnken <terrencemm2@gmail.com>
2020-02-26 14:50:29 -0600

    adds setTimeout to display new question, displays timer and highscore

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,23 @@
 var questionDisplay = document.getElementById("question");
 var answersDisplay = document.getElementById("answers");
-var correctDisplay = document.getElementById("correct-answer");
+var highScoreDisplay = document.getElementById("high-score");
+var timerDisplay = document.getElementById("timer");
+var startButton = document.getElementById("start-button");
+
+var timer = 60;
+var highScore;
 var correctAnswer = "";
 
 function randomQuestion(arr) {
     return arr[Math.floor(Math.random() * arr.length)];
-}
+};
 
 function displayQuestion() {
+    clearTimeout();
+
+    questionDisplay.innerHTML = "";
+    answersDisplay.innerHTML = "";
+
     var set = randomQuestion(data);
     correctAnswer = set.correctAnswer;
 
@@ -18,19 +28,39 @@ function displayQuestion() {
     for (var i = 0; i < set.answers.length; i++) {
         var answerDiv = document.createElement("div");
         answerDiv.setAttribute("data-answer", set.answers[i]);
+        answerDiv.setAttribute("class", "answer");
         answerDiv.innerText = set.answers[i];
         answersDisplay.appendChild(answerDiv);
     }
-}
+};
 
 function selectedAnswer(event) {
     if (event.target.dataset.answer === correctAnswer) {
         event.target.setAttribute("style", "color: green");
+        setTimeout(displayQuestion, 1000);
     } else {
         event.target.setAttribute("style", "color: red");
+        setTimeout(displayQuestion, 1000);
     }
+};
+
+if (localStorage.getItem("high-score")) {
+    highScore = localStorage.getItem("high-score");
+} else {
+    highScore = 0;
 }
 
-document.addEventListener("click", selectedAnswer);
+highScoreDisplay.innerHTML = highScore;
+
+startButton.addEventListener("click", function() {
+    timerDisplay.innerHTML = timer;
+    startButton.style.display = "none";
+    setInterval(function() {
+        timer--;
+        timerDisplay.innerHTML = timer;
+    }, 1000)
+});
+
+answersDisplay.addEventListener("click", selectedAnswer);
 
 displayQuestion();
