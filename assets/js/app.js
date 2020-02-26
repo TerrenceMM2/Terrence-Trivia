e45cb900149570ca52832cafd12c59e4e00d9aa1
Terrence Mahnken <terrencemm2@gmail.com>
2020-02-26 15:15:21 -0600

    adds and displays timer

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,14 @@
 var questionDisplay = document.getElementById("question");
 var answersDisplay = document.getElementById("answers");
 var highScoreDisplay = document.getElementById("high-score");
+var currentScoreDisplay = document.getElementById("current-score");
 var timerDisplay = document.getElementById("timer");
 var startButton = document.getElementById("start-button");
 
-var timer = 60;
+var timerInt = 5;
+var timer;
 var highScore;
+var currentScore = 0;
 var correctAnswer = "";
 
 function randomQuestion(arr) {
@@ -37,9 +40,13 @@ function displayQuestion() {
 function selectedAnswer(event) {
     if (event.target.dataset.answer === correctAnswer) {
         event.target.setAttribute("style", "color: green");
+        currentScore = currentScore + 5;
+        currentScoreDisplay.innerHTML = currentScore;
         setTimeout(displayQuestion, 1000);
     } else {
         event.target.setAttribute("style", "color: red");
+        currentScore = currentScore - 3;
+        currentScoreDisplay.innerHTML = currentScore;
         setTimeout(displayQuestion, 1000);
     }
 };
@@ -51,13 +58,18 @@ if (localStorage.getItem("high-score")) {
 }
 
 highScoreDisplay.innerHTML = highScore;
+currentScoreDisplay.innerHTML = currentScore;
 
 startButton.addEventListener("click", function() {
-    timerDisplay.innerHTML = timer;
+    timerDisplay.innerHTML = timerInt;
     startButton.style.display = "none";
-    setInterval(function() {
-        timer--;
-        timerDisplay.innerHTML = timer;
+    timer = setInterval(function() {
+        timerInt--;
+        timerDisplay.innerHTML = timerInt;
+        if (timerInt === 0) {
+            questionDisplay.innerHTML = "<h1>GAME OVER!</h1>";
+            clearInterval(timer);
+        }
     }, 1000)
 });
 
