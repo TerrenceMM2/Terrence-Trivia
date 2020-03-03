7d3c06a42803d4539b5d9479f97ebe4f6b05020d
Terrence Mahnken <terrencemm2@gmail.com>
2020-03-02 21:23:33 -0600

    adds styling to buttons and adds more questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -28,5 +28,145 @@ const data = [
             "Japanese"
         ],
         correctAnswer: "Japanese"
+    },
+    {
+        question: "In a past life, I was a professional musician. What instrument did I play?",
+        answers: [
+            "Guitar",
+            "Piano",
+            "Drum Kit",
+            "Didgeridoo"
+        ],
+        correctAnswer: "Drum Kit"
+    },
+    {
+        question: "Which of these artists is NOT a favorite of mine?",
+        answers: [
+            "Queen",
+            "Billy Joel",
+            "Journey",
+            "Daft Punk"
+        ],
+        correctAnswer: "Journey"
+    },
+    {
+        question: "Which of these TV shows is NOT a favorite of mine?",
+        answers: [
+            "Scrubs",
+            "Friends",
+            "ER",
+            "Top Gear"
+        ],
+        correctAnswer: "ER"
+    },
+    {
+        question: "Which of these movies is NOT a favorite of mine?",
+        answers: [
+            "Emperor's New Groove",
+            "Goldeneye",
+            "The Holiday",
+            "Saw"
+        ],
+        correctAnswer: "Saw"
+    },
+    {
+        question: "What foreign country have I visited?",
+        answers: [
+            "China",
+            "Italy",
+            "Japan",
+            "Mexico"
+        ],
+        correctAnswer: "China"
+    },
+    {
+        question: "What is my energy drink of choice?",
+        answers: [
+            "Red Bull",
+            "Monster",
+            "Nos",
+            "Bang"
+        ],
+        correctAnswer: "Red Bull"
+    },
+    {
+        question: "What is my favorite Yankee Candle scent?",
+        answers: [
+            "Autumn Leaves",
+            "Macintosh",
+            "Clean Cotton",
+            "Eucalyptus"
+        ],
+        correctAnswer: "Clean Cotton"
+    },
+    {
+        question: "What is my guilty pleasure?",
+        answers: [
+            "Listening to N'Sync",
+            "Watching Judge Judy",
+            "Perusing TMZ",
+            "Stalking social media"
+        ],
+        correctAnswer: "Watching Judge Judy"
+    },
+    {
+        question: "What did I want to be when I grew up?",
+        answers: [
+            "Astronaut",
+            "Doctor",
+            "Meteorologist",
+            "Firefighter"
+        ],
+        correctAnswer: "Meteorologist"
+    },
+    {
+        question: "What is my favorite holiday?",
+        answers: [
+            "Thanksgiving",
+            "Christmas",
+            "Halloween",
+            "4th of July"
+        ],
+        correctAnswer: "Thanksgiving"
+    },
+    {
+        question: "Which theme park was I previously employed at?",
+        answers: [
+            "Disney",
+            "Seaworld",
+            "Busch Gardens",
+            "Universal Studios"
+        ],
+        correctAnswer: "Busch Gardens"
+    },
+    {
+        question: "Which bone have I broken?",
+        answers: [
+            "Wrist",
+            "Collar bone",
+            "Ankle",
+            "Never broken a bone"
+        ],
+        correctAnswer: "Never broken a bone"
+    },
+    {
+        question: "What is my favorite Disney park?",
+        answers: [
+            "Epcot",
+            "Magic Kingdom",
+            "Animal Kingdom",
+            "Hollywood Studios"
+        ],
+        correctAnswer: "Epcot"
+    },
+    {
+        question: "What is my current job title?",
+        answers: [
+            "Application Developer",
+            "Senior Consultant",
+            "Software Engineer",
+            "Network Manager"
+        ],
+        correctAnswer: "Senior Consultant"
     }
 ]
